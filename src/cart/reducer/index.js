0012b4418e51d2d8b3f32a4a33fd4c6f2200f013
Mohit Kumar <mohit.kumar3@hp.com>
2021-11-15 10:06:04 +0530

    update

--- src/cart/reducer/index.js
+++ src/cart/reducer/index.js
@@ -8,41 +8,40 @@ import {
 export const cartLists = (lists = { items: [], total: 0 }, action) => {
   if (action.type === ADD_TO_CART) {
     let priceOfAnItem = action.payload.price;
-    let itemAlreadyPresent = lists.items.some(item => item.name === action.payload.name);
-    if (itemAlreadyPresent) {
-      let itemIndex = lists.items.findIndex(item => item.name === action.payload.name);
-      let newArray = lists.items;
+    let itemIndex = lists.items.findIndex(item => item.name === action.payload.name);
+    if (itemIndex !== -1) {
       let item = lists.items[itemIndex];
       let qty = item.qty + action.payload.qty;
       let totalPrice = qty * item.price;
       let priceAfterDiscount = totalPrice - (totalPrice * item.discount) / 100;
-      newArray[itemIndex] = { ...item, qty, totalPrice, priceAfterDiscount };
+      lists.items[itemIndex] = { ...item, qty, totalPrice, priceAfterDiscount };
       return {
         ...lists,
-        items: newArray,
+        items: lists.items,
         total: +lists.total + +(priceOfAnItem - (priceOfAnItem * action.payload.discount) / 100),
       };
     } else {
-      let totalPrice = priceOfAnItem;
-      let priceAfterDiscount = totalPrice - (totalPrice * action.payload.discount) / 100;
+      let priceAfterDiscount = priceOfAnItem - (priceOfAnItem * action.payload.discount) / 100;
       return {
         ...lists,
-        items: [...lists.items, { ...action.payload, totalPrice, priceAfterDiscount }],
+        items: [
+          ...lists.items,
+          { ...action.payload, totalPrice: priceOfAnItem, priceAfterDiscount },
+        ],
         total: +lists.total + +priceAfterDiscount,
       };
     }
   } else if (action.type === REMOVE_SINGLE_QTY_FROM_CART) {
     let priceOfAnItem = action.payload.price;
     let itemIndex = lists.items.findIndex(item => item.name === action.payload.name);
-    let newArray = lists.items;
     let item = lists.items[itemIndex];
     let qty = item.qty - action.payload.qty;
     let totalPrice = qty * item.price;
     let priceAfterDiscount = totalPrice - (totalPrice * item.discount) / 100;
-    newArray[itemIndex] = { ...item, qty, totalPrice, priceAfterDiscount };
+    lists.items[itemIndex] = { ...item, qty, totalPrice, priceAfterDiscount };
     return {
       ...lists,
-      items: newArray,
+      items: lists.items,
       total: +lists.total - +(priceOfAnItem - (priceOfAnItem * action.payload.discount) / 100),
     };
   } else if (action.type === CLEAR_CART) {
